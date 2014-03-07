4bc3284d7547e4ca74bc6e335795c54274d5052e
Sam Alloing <sam.alloing@gmail.com>
2014-03-07 13:17:07 +0100

    deleted old files

--- Root_progs/importeer.bak.php
+++
@@ -1,3 +0,0 @@
-<?php
-exec("php controle_thesaurus_csv.php > /www/libis/vol03/collectiveaccess/crkc_media/controle_thesaurus_csv.log 2> /www/libis/vol03/collectiveaccess/crkc_media/error.txt ");
-?>
--- Root_progs/reindex_table.bak.php
+++
@@ -1,3 +0,0 @@
-<?php
-exec("php support/utils/reindex.php 'http://crkc.be.halotest.cc.kuleuven.be/ca_crkc' 'ca_objects' > /www/libis/vol03/collectiveaccess/crkc_media/reindex.log 2> /www/libis/vol03/collectiveaccess/crkc_media/reindex_error.txt ");
-?>
