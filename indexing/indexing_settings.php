c80b582cb7cbd803338ed538a6008b7357fd6c58
Naeem <naeem.muhammad@libis.kuleuven.be>
2014-03-19 12:16:45 +0100

    crkc indexing scripts

---
+++ indexing/indexing_settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: NaeemM
+ * Date: 19/03/14
+ */
+
+$_SERVER['SCRIPT_FILENAME'] = '/www/libis/web/lias_html/crkcsandbox_test/index.php';
+putenv('COLLECTIVEACCESS_HOME=/www/libis/web/lias_html/crkcsandbox_test/');
+ini_se('memory_limit', '1024M');
+set_time_limit(0);
