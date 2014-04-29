b6b8ff34547d634228a39599676d2dfdaa6e79b8
Naeem <naeem.muhammad@libis.kuleuven.be>
2014-04-29 13:29:09 +0200

    include file name correction

--- indexing/reindex_sort.php
+++ indexing/reindex_sort.php
@@ -1,11 +1,10 @@
 <?php
 /**
-
- *
+ * User: NaeemM
  * Date: 19/03/14
  */
 
-require_once('indexing_setting.php');
+require_once('indexing_settings.php');
 
 exec("nohup sh reindex_sort.sh > ".$media_directory."/reindex_ca_crkc_sort.txt 2> ".$media_directory."/reindex_ca_crkc_sort.txt.err < /dev/null &");
 
