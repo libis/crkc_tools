1452b358696b0f5114edffe93ac5779aae3a36bc
Naeem <naeem.muhammad@libis.kuleuven.be>
2014-05-28 15:24:19 +0200

    adapted for icts queuing server

--- Queuing/pdfworker.php
+++ Queuing/pdfworker.php
@@ -3,16 +3,17 @@
  * User: NaeemM
  * Date: 13/03/14
  *
- * This script generates pdfs of contents received through rabbitmq queues
+ * This script generates pdfs of contents received through rabbitmq queues.
  */
 
-require_once __DIR__ . '/queue_server_conf.php';
+require_once __DIR__ . '/queue_server_conf.php';        //queuing server settings
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/phpmailer/PHPMailerAutoload.php';
 
 use PhpAmqpLib\Connection\AMQPConnection;
 
-$connection = new AMQPConnection($co_rabbit_mq_server, $co_rabbit_mq_port, $co_rabbit_mq_uid, $co_rabbit_mq_pwd);
+$connection = new AMQPConnection($co_rabbit_mq_server, $co_rabbit_mq_port, $co_rabbit_mq_uid, $co_rabbit_mq_pwd, $co_rabbit_mq_vhost);
+
 $channel = $connection->channel();
 
 $channel->queue_declare($co_queue_name, false, false, false, false);
@@ -20,19 +21,22 @@ $channel->queue_declare($co_queue_name, false, false, false, false);
 echo " [*] Waiting for requests to generate pdfs. To exit press CTRL+C", "\n";
 
 $callback = function($msg) {
-    require_once __DIR__ . '/pdf_worker_conf.php';
+    require __DIR__ . '/pdf_worker_conf.php';
 
     $microtime = round(microtime(true) * 1000);
-    $va_base_directory = dirname(__FILE__)."/pdfdownload/";
+    $va_base_directory = dirname(__FILE__)."/".$co_pdf_base_dir."/";
     $va_request_directory = $va_base_directory.$microtime;
 
     if (!file_exists($va_request_directory)) {
         mkdir($va_request_directory);
     }
 
-    $va_content_file = $va_request_directory."/".date("m_d_y")."_".$microtime.".html";
-    $va_pdf_file = $va_request_directory."/".date("m_d_y")."_".$microtime.".pdf";
-    $va_pdf_download_link = $co_base_url.date("m_d_y")."_".$microtime.".pdf";
+    $file_name = date("m_d_y")."_".$microtime;
+    $va_content_file = $va_request_directory."/".$file_name.".html";
+    $va_pdf_file = $va_request_directory."/".$file_name.".pdf";
+
+    $temPath = explode(basename(__DIR__),$va_pdf_file);
+    $va_pdf_download_link = $co_base_url."/".basename(__DIR__).$temPath[1];
 
     $va_pdf_message = json_decode($msg->body);
     $va_pdf_contents = array_key_exists('pdf_contents', $va_pdf_message) ? $va_pdf_message->pdf_contents : '' ;
@@ -68,17 +72,18 @@ $callback = function($msg) {
 
             $va_user_name = isset($va_pdf_message->user_info->name)? $va_pdf_message->user_info->name : '';
 
-            $mail->addAddress("naeemmuhammad@gmail.com", $va_user_name);  // Add a recipient
+            $mail->addAddress($va_pdf_message->user_info->email, $va_user_name);  // Add a recipient
 
             $mail->isHTML(true);
-            $mail->Subject = "Collective Access Record Search PDF";
+            $mail->Subject = "=?UTF-8?B?".base64_encode("Uw geëxporteerde CRKC pdf is klaar")."?=";
 
             $va_email_message = ($va_ret_val === 0)
                 ?
-                "Dear ".$va_user_name. ",<br><br>".
-                "Requested pdf is available at:<br>".
+                "Beste ".$va_user_name. ",<br><br>".
+                "Uw pdf is beschikbaar op:<br>".
                 $va_pdf_download_link."<br><br>".
-                "Total pdf generation time (seconds): ".(int)($endtime-$microtime)/1000
+                "Met vriendelijke groeten,<br>".
+                "Team CRKC-LIBIS<br>"
                 :
                 "Error in generating pdf, please try again";
 
